342471f3147932e2b3e68fcb50d3f41a89668c86
Krzysztof Hager <dsw52687@student.dsw.edu.pl>
2024-06-04 21:36:12 +0200

    Zaktualizowano help o informację o nowych i krótkich flagach

--- skrypt.sh
+++ skrypt.sh
@@ -7,7 +7,9 @@ help(){
     printf "Dostępne flagi:\n"
     printf -- "--help -h Wyświetla tę wiadomość.\n"
     printf -- "--date -d Wyświetla aktualną datę.\n"
-    printf -- "--logs -l <liczba> Tworzy folder logs oraz <liczba> plików logs/log<i>.txt, zawierających informacie o dacie, skrypcie oraz numerze loga. Jeśli <liczba> nie zostanie podana lub argument będzie niepoprawny, zostanie domyślnie utworzone 100 logów.\n\n"
+    printf -- "--logs -l <liczba> Tworzy <liczba> plików ./log<i>.txt, zawierających informacie o dacie, skrypcie oraz numerze loga. Jeśli <liczba> nie zostanie podana lub argument będzie niepoprawny, zostanie domyślnie utworzone 100 logów.\n"
+    printf -- "--init Klonuje repozytorium do aktualnej lokalizacji oraz dopisuje ją (tymczasowo) do PATH\n"
+    printf -- "--error -e <liczba> Tworzy <liczba> plików i folderó∑ ./error<i>/error<i>.txt, zawierających informacie o dacie, skrypcie oraz numerze loga. Jeśli <liczba> nie zostanie podana lub argument będzie niepoprawny, zostanie domyślnie utworzone 100 logów.\n\n"
 }
 
 logs(){
