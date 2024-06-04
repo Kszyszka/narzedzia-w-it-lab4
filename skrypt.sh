2fce339d839ce1e25e0391d027fa0d6173d08e0b
Krzysztof Hager <dsw52687@student.dsw.edu.pl>
2024-06-04 19:53:35 +0200

    Dodano funkcjonalność --logs

--- skrypt.sh
+++ skrypt.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 printf "Skrypt na zaliczenie 4 labolatorium z Narzędzi pracy w branży IT - Krzysztof Hager 52687\n\n"
 
+logs(){
+    mkdir logs
+    # Logi w osobnym folderze dla zachowania porządku
+
+    for i in {1..100}
+    do
+        name="log$i.txt"
+        printf "$name\n$0\n$(date)" > "./logs/$name"
+    done
+}
+
 case "$1" in
     "--date") date ;;
-    *) command ... ;;
+    "--logs") logs;;
+    *) printf "Nieznana flaga." ;;
 esac
