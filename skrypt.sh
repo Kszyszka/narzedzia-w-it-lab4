cb198f4252224323575c7e3ec8cf68c487a27581
Krzysztof Hager <dsw52687@student.dsw.edu.pl>
2024-06-04 20:45:27 +0200

    Dodano funkcjonalność --help

--- skrypt.sh
+++ skrypt.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 printf "Skrypt na zaliczenie 4 labolatorium z Narzędzi pracy w branży IT - Krzysztof Hager 52687\n\n"
 
+help(){
+    printf "skrypt.sh - Skrypt na zaliczenie: Krzysztof Hager 52687\n\n"
+    printf "Użycie: ./skrypt.sh <flaga> <opcjonalny argument>\n\n"
+    printf "Dostępne flagi:\n"
+    printf -- "--help Wyświetla tę wiadomość.\n"
+    printf -- "--date Wyświetla aktualną datę.\n"
+    printf -- "--logs <liczba> Tworzy folder logs oraz <liczba> plików logs/log<i>.txt, zawierających informacie o dacie, skrypcie oraz numerze loga. Jeśli <liczba> nie zostanie podana lub argument będzie niepoprawny, zostanie domyślnie utworzone 100 logów.\n\n"
+}
+
 logs(){
     mkdir -p logs
     # Logi w osobnym folderze dla zachowania porządku
@@ -23,5 +32,6 @@ logs(){
 case "$1" in
     "--date") date ;;
     "--logs") logs "$2";;
-    *) printf "Nieznana flaga." ;;
+    "--help") help ;;
+    *) help ;;
 esac
