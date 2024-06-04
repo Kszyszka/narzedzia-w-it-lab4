68dc270b82399d42e99bebc1013ebef1c5dabebb
Krzysztof Hager <dsw52687@student.dsw.edu.pl>
2024-06-04 20:16:59 +0200

    Dodano obsługę argumentu dla flagi --logs oraz obsługę funkcji bez podania argumentu

--- skrypt.sh
+++ skrypt.sh
@@ -2,10 +2,18 @@
 printf "Skrypt na zaliczenie 4 labolatorium z Narzędzi pracy w branży IT - Krzysztof Hager 52687\n\n"
 
 logs(){
-    mkdir logs
+    mkdir -p logs
     # Logi w osobnym folderze dla zachowania porządku
 
-    for i in {1..100}
+    if [ "$1" -gt 0 -a "$1" =~ "^[0-9]+$" ]; then
+        index="$1"
+    else
+        index=100
+    fi
+
+    mkdir -p logs
+
+    for i in $(seq 1 $index)
     do
         name="log$i.txt"
         printf "$name\n$0\n$(date)" > "./logs/$name"
@@ -14,6 +22,6 @@ logs(){
 
 case "$1" in
     "--date") date ;;
-    "--logs") logs;;
+    "--logs") logs "$2";;
     *) printf "Nieznana flaga." ;;
 esac
